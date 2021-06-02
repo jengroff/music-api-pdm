128bd0ca9a772fafcab668922ede6e06ef61a776
Josh Engroff <jengroff@alumni.princeton.edu>
2021-06-02 03:15:33 -0400

    updated status codes for endpoints where they were missing

--- project/app/routes/playlists.py
+++ project/app/routes/playlists.py
@@ -20,22 +20,23 @@ async def create_playlist(playlist: PlaylistInsertSchema):
     return await Playlist_Pydantic.from_tortoise_orm(playlist_obj)
 
 
-@router.get("/playlists/{id}", response_model=Playlist_Pydantic, responses={404: {"model": HTTPNotFoundError}})
+@router.get("/playlists/{id}", response_model=Playlist_Pydantic, status_code=200,
+            responses={404: {"model": HTTPNotFoundError}})
 async def get_playlist(id: int):
     return await Playlist_Pydantic.from_queryset_single(Playlists.get(id=id))
 
 
-@router.put("/playlists/{id}", response_model=Playlist_Pydantic, responses={404: {"model": HTTPNotFoundError}})
+@router.put("/playlists/{id}", response_model=Playlist_Pydantic, status_code=200,
+            responses={404: {"model": HTTPNotFoundError}})
 async def update_playlist(id: int, playlist: PlaylistInsertSchema):
     await Playlists.filter(id=id).update(**playlist.dict(exclude_unset=True))
     return await Playlist_Pydantic.from_queryset_single(Playlists.get(id=id))
 
 
-@router.delete("/playlists/{id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+@router.delete("/playlists/{id}", response_model=Status, status_code=200,
+               responses={404: {"model": HTTPNotFoundError}})
 async def delete_playlist(id: int):
     deleted_count = await Playlists.filter(id=id).delete()
     if not deleted_count:
         raise HTTPException(status_code=404, detail=f"Playlist {id} not found")
     return Status(message=f"Deleted playlist {id}")
-
-
